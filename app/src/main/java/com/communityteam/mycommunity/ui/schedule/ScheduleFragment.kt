c597025dd1f55b68a59812e21d90a1d6d6d0795d
Frank <f.chen4978637@gmail.com>
2022-05-23 23:03:23 -0400

    add binding and viewModel for ScheduleFragment

--- app/src/main/java/com/communityteam/mycommunity/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/communityteam/mycommunity/ui/schedule/ScheduleFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.communityteam.mycommunity.R
+import com.communityteam.mycommunity.databinding.FragmentScheduleBinding
+import com.communityteam.mycommunity.repository.ViewModelFactory
 
 class ScheduleFragment : Fragment() {
+
+    private val viewModel: ScheduleViewModel by lazy {
+        ViewModelProvider(this, ViewModelFactory()).get(ScheduleViewModel::class.java)
+    }
+
+    private var _binding: FragmentScheduleBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        _binding = FragmentScheduleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/communityteam/mycommunity/ui/schedule/ScheduleViewModel.kt
@@ -0,0 +1,10 @@
+package com.communityteam.mycommunity.ui.schedule
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.communityteam.mycommunity.repository.ViewModelFactory
+
+class ScheduleViewModel : ViewModel() {
+
+
+}
