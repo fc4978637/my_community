5527d2901bcea343167059585cb56adaaf07d934
Frank <f.chen4978637@gmail.com>
2022-05-23 23:02:48 -0400

    add event model

---
+++ app/src/main/java/com/communityteam/mycommunity/model/Event.kt
@@ -0,0 +1,10 @@
+package com.communityteam.mycommunity.model
+
+import java.time.LocalDateTime
+import java.util.*
+
+data class Event(
+    val title: String,
+    val date: String,
+    val content: String
+)
