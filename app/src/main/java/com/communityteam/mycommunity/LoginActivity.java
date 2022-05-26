a431e569816b390bc063c24d62da3bbbbb8bcf55
dianawu5 <yaqiongwu5@gmail.com>
2022-05-25 17:10:29 -0700

    Add Register Button Action
    Switch to RegisterActivity by clicking Register Button

--- app/src/main/java/com/communityteam/mycommunity/LoginActivity.java
+++ app/src/main/java/com/communityteam/mycommunity/LoginActivity.java
@@ -16,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView username;
     TextView password;
     Button loginbtn;
+    Button registerbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         username = (TextView) findViewById(R.id.et_username);
         password = (TextView) findViewById(R.id.et_password);
         loginbtn = (Button) findViewById(R.id.button_login);
+        registerbtn = (Button) findViewById(R.id.button_register);
 
        /* when the login button is clicked, we first check if user name is valid, if username is not valid, return username doesn't extist;
         then check if password matches, if doesn't match, return password and username not match;
@@ -35,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // first check username, password, then direct to MainActivity and show different Dashboard and Calendar based on user type
-               /*  below is a test. To run this test, also need to set this LoginActivity as LAUNCH activity in AndroidManifest.xml
+                 //below is a test. To run this test, also need to set this LoginActivity as LAUNCH activity in AndroidManifest.xml
 
                if(username.getText().toString().equals("peter") && password.getText().toString().equals("peter123")){
                     Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
@@ -47,7 +49,15 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "username doesn't exist", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(LoginActivity.this, "username and password not match", Toast.LENGTH_LONG).show();
-                }*/
+                }
+            }
+        });
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
